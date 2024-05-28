af699c9d610244365543e57fcf57e9f64e75a506
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-05-29 00:41:30 +0530

    added logger

--- src/main/java/com/example/config/MyAuthorizationManagerPost.java
+++ src/main/java/com/example/config/MyAuthorizationManagerPost.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.authorization.method.MethodInvocationResult;
@@ -10,9 +12,13 @@ import java.util.function.Supplier;
 
 @Component
 public class MyAuthorizationManagerPost implements AuthorizationManager<MethodInvocationResult> {
+
+    private final Logger logger = LoggerFactory.getLogger(MyAuthorizationManagerPost.class);
+
     @Override
     public AuthorizationDecision check(Supplier<Authentication> authentication, MethodInvocationResult methodInvocationResult) {
         // ... authorization logic
+        logger.info("MyAuthorizationManagerPost {}", authentication.get());
         return new AuthorizationDecision(true);
     }
 }
--- src/main/java/com/example/config/MyAuthorizationManagerPre.java
+++ src/main/java/com/example/config/MyAuthorizationManagerPre.java
@@ -1,6 +1,8 @@
 package com.example.config;
 
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
@@ -10,9 +12,13 @@ import java.util.function.Supplier;
 
 @Component
 public class MyAuthorizationManagerPre implements AuthorizationManager<MethodInvocation> {
+
+    private final Logger logger = LoggerFactory.getLogger(MyAuthorizationManagerPre.class);
+
     @Override
     public AuthorizationDecision check(Supplier<Authentication> authentication, MethodInvocation invocation) {
         // ... authorization logic
+        logger.info("MyAuthorizationManagerPre {}", authentication.get());
         return new AuthorizationDecision(true);
     }
 }
